884ed14e353abd083bb58d2d9a30d6e627af74a2
p0ja <ajopek@tlen.pl>
2017-04-02 22:58:00 +0200

    Merge branch 'master' into develop
    # Conflicts:
    #	src/AppBundle/AppBundle.php

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mackfish
+ * Date: 2017-04-02
+ * Time: 22:01
+ */
+
+namespace AppBundle\Entity;
+
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", unique=true)
+     */
+    private $email;
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword()
+    {
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+}
