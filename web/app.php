35ee5d0101c2c5cd990ae40a8d7e464ca8b10b97
p0ja <ajopek@tlen.pl>
2019-12-16 10:12:53 +0100

    upgraded symfony bundle

--- web/app.php
+++ web/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\HttpFoundation\Request;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require __DIR__ . '/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
